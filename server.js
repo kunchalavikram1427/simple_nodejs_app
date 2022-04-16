cb5ca211a478846614c6dc0f44adbde9c6bb04e7
Vikram Babu <dme.weekend.batch01.drive01@gmail.com>
2022-04-17 00:22:28 +0530

    init

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
-'use strict';
-
 const express = require('express');
+const os = require('os');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
+// Returns a fixed string
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
+});
+
+// Returns the hostname of the instance/container/pod
+app.get('/gethostname', (req, res) => {
+  console.log(os.hostname());
+  res.send(os.hostname());
 });
 
 app.listen(PORT, HOST);
